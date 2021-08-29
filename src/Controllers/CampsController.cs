520e10bb022addf9f5ea3a2d3b144f64bfe6e247
mojeda86 <matias_goc@hotmail.com>
2021-08-29 00:08:13 -0300

    End of Module 3

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository campRepository;
+        private readonly IMapper mapper;
+
+        public CampsController(ICampRepository campRepository, IMapper mapper)
+        {
+            this.campRepository = campRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CampModel>>> Get(bool includeTalks = false)
+        {
+            try
+            {
+                var camps = await campRepository.GetAllCampsAsync(includeTalks);
+                List<CampModel> modelsToReturn = mapper.Map<List<CampModel>>(camps);
+                return (modelsToReturn);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Something wrong happened from our side");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var camp = await campRepository.GetCampAsync(moniker);
+                if (camp == null) return NotFound();
+                return mapper.Map<CampModel>(camp);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Something wrong happened");
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<List<CampModel>>> Get(DateTime theDate, bool includeTalks=false)
+        {
+            try
+            {
+                var camp = await campRepository.GetAllCampsByEventDate(theDate, includeTalks);
+                if (camp == null) return NotFound();
+                return mapper.Map<List<CampModel>>(camp);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Something wrong happened");
+            }
+        }
+    }
+}
---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c => c.VenueName, o => o.MapFrom(c => c.Location.VenueName))
+                .ForMember(c => c.Address1, o => o.MapFrom(c => c.Location.Address1))
+                .ForMember(c => c.Address2, o => o.MapFrom(c => c.Location.Address2))
+                .ForMember(c => c.Address3, o => o.MapFrom(c => c.Location.Address3))
+                .ForMember(c => c.CityTown, o => o.MapFrom(c => c.Location.CityTown))
+                .ForMember(c => c.StateProvince, o => o.MapFrom(c => c.Location.StateProvince))
+                .ForMember(c => c.PostalCode, o => o.MapFrom(c => c.Location.PostalCode))
+                .ForMember(c => c.Country, o => o.MapFrom(c => c.Location.Country));
+
+            this.CreateMap<Talk, TalkModel>();
+            this.CreateMap<Speaker, SpeakerModel>();
+
+        }
+    }
+}
